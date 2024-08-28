864edf499811a7c092256bbcca4d155f1d99be89
BND <125670651+benadoBND@users.noreply.github.com>
2024-08-27 21:39:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Calculadora calc = new Calculadora();
+Console.WriteLine("Informe o valor de x: ");
+double x = Convert.ToDouble (Console.ReadLine());
+
+Console.WriteLine("Informe o valor de y: ");
+double y = Convert.ToDouble (Console.ReadLine());
+
+double resultadoSoma = calc.Soma(x,y);
+Console.WriteLine($"O resultado da soma de {x} e {y} é igual a : {resultadoSoma}");
+
+Console.WriteLine($"O resultado  da subtração é: {calc.Subtracao(x,y)}");
+
+Console.WriteLine($"O resultado da multiplicação é: {calc.Multiplicar(x,y)}");
+
+Console.WriteLine($"O resultado da divisão é: {calc.Divisao(x,y)}");
